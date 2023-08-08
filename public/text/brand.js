78ae5bae461fc61da4f19c45632ad62095fb918e
EISAGHANI <eisabacha.com>
2023-08-08 21:51:00 +0200

    first

---
+++ public/text/brand.js
@@ -0,0 +1,20 @@
+import imgAPI from '~/public/images/imgAPI';
+
+const brand = {
+  femine: {
+    name: 'Mohammad Eisa',
+    title: 'Software Engineer',
+    desc: 'Personal Profile - This is my personal website',
+    prefix: 'Eisa',
+    footerText: 'Eisa Theme 2023',
+    logoText: 'Eisa Profile',
+    projectName: 'Profile',
+    url: 'Iluxi.ux-maestro.com/profile',
+    img: '/images/femine-logo.png',
+    avatar: imgAPI.femine[0],
+    cover: imgAPI.femine[1],
+    notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+};
+
+export default brand;
